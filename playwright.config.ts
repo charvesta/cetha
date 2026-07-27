f08e20819d53d77faf8879767ff13c8947f42c13
UyyeyMan <abangbakti2@gmail.com>
2026-07-28 00:01:44 +0700

    test: make visual baselines cross-platform

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,12 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.03,
+    },
+  },
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: 'http://127.0.0.1:4325',
